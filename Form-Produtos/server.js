bf9d436522b24ecf68d92baa73f177feec77d9e4
SamuelLEal <samuleal007@gmail.com>
2025-05-16 00:06:39 -0300

    Commit

---
+++ Form-Produtos/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'form.html'));
+});
+app.post('/cadastro', (req, res) => {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        const dados = {};
+        body.split('&').forEach(par => {
+            const [chave, valor] = par.split('=');
+            dados[chave] = decodeURIComponent(valor.replace(/\+/g, ' '));
+        });
+        res.send(`
+      <h2>Produto cadastrado com sucesso!</h2>
+      <p><strong>Nome:</strong> ${dados.nome}</p>
+      <p><strong>Preço:</strong> R$ ${dados.preco}</p>
+      <a href="/">Voltar</a>
+    `);
+
+        console.log('Dados recebidos:', dados);
+    });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
